1f29adc77f9c6d07994e449830f229dcc7d6eef2
jamalfsi <jamalfsi@gmail.com>
2023-10-08 13:22:47 +0300

    header link and created component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,20 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from './pages/Home'
+import SignIn from "./pages/Signin";
+import SignUp from "./pages/SignUp";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
+import Header from "./components/Header";
 
-const App = () => {
-  return (<h1 className='text-blue-600'>App</h1>)
+export default function App() {
+  return <BrowserRouter>
+   <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
 }
-
-export default App
